204e517e9610301c0d61b75f86dd349ae9056a4c
Troy Fitzwater <troyfitzwater@protonmail.com>
2023-06-10 13:39:20 -0400

    adding OpenAI configs adding logic for AI chat removing logic for bot template adding demo prompt configs for chefbot

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
 // Import required packages
 import * as restify from "restify";
+import { Application, ConversationHistory, DefaultPromptManager, DefaultTurnState, OpenAIModerator, OpenAIPlanner, AI } from '@microsoft/teams-ai';
+import path from "path";
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
@@ -8,10 +10,9 @@ import {
   ConfigurationServiceClientCredentialFactory,
   ConfigurationBotFrameworkAuthentication,
   TurnContext,
+  MemoryStorage,
 } from "botbuilder";
 
-// This bot's main dialog.
-import { TeamsBot } from "./teamsBot";
 import config from "./config";
 
 // Create adapter.
@@ -52,19 +53,67 @@ const onTurnErrorHandler = async (context: TurnContext, error: Error) => {
 // Set the onTurnError for the singleton CloudAdapter.
 adapter.onTurnError = onTurnErrorHandler;
 
-// Create the bot that will handle incoming messages.
-const bot = new TeamsBot();
-
 // Create HTTP server.
 const server = restify.createServer();
 server.use(restify.plugins.bodyParser());
+
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-  console.log(`\nBot Started, ${server.name} listening to ${server.url}`);
+  console.log(`\n${server.name} listening to ${server.url}`);
+  console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
+  console.log('\nTo test your bot in Teams, sideload the app manifest.json within Teams Apps.');
 });
 
-// Listen for incoming requests.
-server.post("/api/messages", async (req, res) => {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface ConversationState {}
+type ApplicationTurnState = DefaultTurnState<ConversationState>;
+
+// Create AI components
+const planner = new OpenAIPlanner({
+  apiKey: config.openAIKey,
+  defaultModel: 'text-davinci-003',
+  logRequests: true
+});
+const moderator = new OpenAIModerator({
+  apiKey: config.openAIKey,
+  moderate: 'both'
+});
+const promptManager = new DefaultPromptManager(path.join(__dirname, './prompts' ));
+
+// Define storage and application
+const storage = new MemoryStorage();
+const app = new Application<ApplicationTurnState>({
+  storage,
+  ai: {
+      planner,
+      moderator,
+      promptManager,
+      prompt: 'chat',
+      history: {
+          assistantHistoryType: 'text'
+      }
+  }
+});
+
+app.ai.action(AI.FlaggedInputActionName, async (context, state, data) => {
+  await context.sendActivity(`I'm sorry your message was flagged: ${JSON.stringify(data)}`);
+  return false;
+});
+
+app.ai.action(AI.FlaggedOutputActionName, async (context, state, data) => {
+  await context.sendActivity(`I'm not allowed to talk about such things.`);
+  return false;
+});
+
+app.message('/history', async (context, state) => {
+  const history = ConversationHistory.toString(state, 2000, '\n\n');
+  await context.sendActivity(history);
+});
+
+// Listen for incoming server requests.
+server.post('/api/messages', async (req, res) => {
+  // Route received a request to adapter for processing
   await adapter.process(req, res, async (context) => {
-    await bot.run(context);
+    // Dispatch to application for routing
+    await app.run(context);
   });
 });
